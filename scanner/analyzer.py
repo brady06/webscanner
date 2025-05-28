8b6c7a05dbe6bc2c82537fae6dbed7b7e57654b2
brady06 <bradygraff@gmail.com>
2025-05-28 19:45:02 -0400

    started analyzer and added issue class

---
+++ scanner/analyzer.py
@@ -0,0 +1,12 @@
+# Imports
+import crawler
+
+# Returns a list of marked issues and their locations (url)
+def analyze_site(url):
+    # Crawl the site for all relevent links
+    response_list = crawler.crawl_site(url)
+
+    # Issues list to append to
+
+
+    # main analysis logic
---
+++ scanner/issue.py
@@ -0,0 +1,7 @@
+# Imports
+
+# Object that stores issue data and methods
+class Issue:
+    def __init__(self, issue, url):
+        self.issue = issue
+        self.url = url
