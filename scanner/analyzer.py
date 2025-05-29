a5648d28ec0d129459376fcfd90a665bd6fc53ea
brady06 <bradygraff@gmail.com>
2025-05-28 20:17:59 -0400

    added check_security_headers

--- scanner/analyzer.py
+++ scanner/analyzer.py
@@ -1,5 +1,6 @@
 # Imports
 import crawler
+import issue
 
 # Returns a list of marked issues and their locations (url)
 def analyze_site(url):
@@ -7,6 +8,30 @@ def analyze_site(url):
     response_list = crawler.crawl_site(url)
 
     # Issues list to append to
-
+    global issues
+    issues = []
 
     # main analysis logic
+    for response in response_list:
+        check_security_headers(response)
+
+    return issues
+
+def check_security_headers(response):
+    if "Content-Security-Policy" not in response.headers:
+        issues.append(issue.Issue("Missing Content-Security-Policy", response.url))
+
+    if response.headers.get("X-Frame-Options", "").upper() not in ["DENY", "SAMEORIGIN"]:
+        issues.append(issue.Issue("Missing or weak X-Frame-Options"))
+
+    if "Strict-Transport-Security" not in response.headers:
+        issues.append(issue.Issue("Missing Strict-Transport-Security"))
+
+    if response.headers.get("X-Content-Type-Options", "").lower() != "nosniff":
+        issues.append(issue.Issue("Missing or incorrect X-Content-Type-Options"))
+
+    if "Referrer-Policy" not in response.headers:
+        issues.append(issue.Issue("Missing Referrer-Policy"))
+
+
+
